d51735f8fdea88751f5ea51c83c5335d0e7ac9b2
Analu <anaguimaraes041@gmail.com>
2026-07-17 23:10:31 -0300

    Adição de eventos básicos - Boas vindas aos membros

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #   BIBLIOTECA
 import discord 
 from discord.ext import commands
-import meuToken.py
-from meuToken.py import meuToken
+import meuToken
+from meuToken import meuToken
 
 #   PERMISSOES
 permissoes = discord.Intents.default()
@@ -22,5 +22,23 @@ async def on_ready():
 async def Gunter(ctx:commands.Context):
     nomeUsuario = ctx.author.display_name
     await ctx.reply(f"to aqui {nomeUsuario} :) ")
+
+# MENSAGEM PARA CRIAÇÃO DE CANAIS
+@gunter.event
+async def on_guild_channel_create(canal:discord.abc.GuildChannel):
+    await canal.send(f"novo canal criado :) ")
+
+# MENSAGEM PARA NOVOS MEMBROS
+@gunter.event
+async def on_member_join(membro:discord.Member):
+    canal = gunter.get_channel(1427064418356695150)
+    await canal.send(f"{membro.display_name}, chegou no servidor!\nSeja bem vindo!")
+
+# MENSAGEM PARA QUANDO MEMBRO SAIR
+@gunter.event
+async def on_member_remove(membro:discord.Member):
+    canal = gunter.get_channel(1427064418356695150)
+    await canal.send(f"{membro.display_name}, saiu do servidor... :(")
+
 
 gunter.run(meuToken)
