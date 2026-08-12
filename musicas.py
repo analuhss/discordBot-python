d43f664f61e4ce82b0ebaee55d5d2a1ab876f363
Ana Luiza <anaguimaraes041@gmail.com>
2026-08-12 10:48:04 -0300

    debug

--- musicas.py
+++ musicas.py
@@ -9,7 +9,7 @@ ydlOptions = {
     "format": "bestaudio/best", #pega a melhor qualidade disponível
     "noplaylist": True, # se o link for de uma playlist, pega só a primeira música
     "quiet" : True, # Não fica printando logs no terminal
-    "default_searach": "ytsearach" #se o usuário digitar um nome em vez de um link, o yt-dlp faz uma busca no Youtube
+    "default_searach": "ytsearach", #se o usuário digitar um nome em vez de um link, o yt-dlp faz uma busca no Youtube
     "source_address": "0.0.0.0", #Ajuste de rede que evita alguns erros de conexão IPv6, comuns com esse tipo de extração
 }
 
@@ -23,24 +23,22 @@ ffmepegOptions = {
 # FILA DE MÚSICAS POR SERVIDOR (id_guild -> lista de url_audio, titulo)
 filas: dict[int, list[tuple[str]]] = {}
 
-def registrarComandosMusica(gunter: discord.Client):
-    # registra os comandos de música na árvore de comandos
+registrarComandosMusica(gunter: discord.Client):
+        # registra os comandos de música na árvore de comandos
 
-def getFila(guild_id: int) -> list: #se o servidor nao tem uma lista ele cria uma
-    if guild_id not in filas:
-        filas[guild_id] = []
-    return filas[guild_id]
+    def getFila(guild_id: int) -> list: #se o servidor nao tem uma lista ele cria uma
+        if guild_id not in filas:
+            filas[guild_id] = []
+        return filas[guild_id]
 
-def tocarProxima(guild: discord.Guild):
-    fila = getFila(guild.id)
-    vc = guild.voice_client
+    def tocarProxima(guild: defdiscord.Guild):
+        fila = getFila(guild.id)
+        vc = guild.voice_client
 
-    if not fila or vc is None:
-        return
+        if not fila or vc is None:
+            return
     
     fonte = discord.FFmpegPCMAudio(url_audio,**ffmepegOptions)
     vc.play(fonte, after=lambda erro: tocarProxima(guild))
 
     # ENTRAR NO CANAL DE VOZ
-
-
